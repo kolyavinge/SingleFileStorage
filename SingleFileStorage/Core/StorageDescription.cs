cdf92ea373417ac90dc969dd9c7178ed04c07a28
Vinge Nikolay <vn2756@gmail.com>
2022-01-07 20:19:02 +0400

    MemoryStorageFileStream

--- SingleFileStorage/Core/StorageDescription.cs
+++ SingleFileStorage/Core/StorageDescription.cs
@@ -5,12 +5,15 @@ namespace SingleFileStorage.Core
 {
     static class StorageDescription
     {
-        public static Memory GetStorageDescription(StorageFileStream fileStream)
+        public static MemoryStorageFileStream GetStorageDescription(StorageFileStream fileStream)
         {
             fileStream.Seek(0, SeekOrigin.Begin);
             var storageDescription = new byte[SizeConstants.StorageDescription];
             fileStream.ReadByteArray(storageDescription, 0, storageDescription.Length);
-            var memory = new Memory(storageDescription);
+            var memory = new MemoryStorageFileStream();
+            memory.Open(Access.Modify);
+            memory.WriteByteArray(storageDescription, 0, storageDescription.Length);
+            memory.Seek(0, SeekOrigin.Begin);
 
             return memory;
         }
